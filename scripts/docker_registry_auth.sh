8b7d6b5f8bda5313c92034512a16c3c6b65b0540
David Dumas <david.dumas@tweag.io>
2022-04-14 13:14:37 +0200

    CI: case with no AWS ECR in Docker config

--- scripts/docker_registry_auth.sh
+++ scripts/docker_registry_auth.sh
@@ -1,8 +1,16 @@
 #!/bin/sh
 set -eu
 
-# Docker JSON configuration for both GitLab container registry and Amazon ECR
+# Docker JSON configuration to access registries
 
 mkdir -pv /root/.docker
 CI_REGISTRY_AUTH=$(printf '%s:%s' "${CI_REGISTRY_USER}" "${CI_REGISTRY_PASSWORD}" | base64 | tr -d '\n')
-echo "{\"auths\":{\"registry.gitlab.com\":{\"auth\":\"${CI_REGISTRY_AUTH}\"}},\"credHelpers\":{\"${AWS_ECR}\":\"ecr-login\"}}" > /root/.docker/config.json
+
+if [ -n "${AWS_ECR:-}" ]
+then
+  # GitLab container registry + AWS ECR (for mirroring)
+  echo "{\"auths\":{\"registry.gitlab.com\":{\"auth\":\"${CI_REGISTRY_AUTH}\"}},\"credHelpers\":{\"${AWS_ECR}\":\"ecr-login\"}}" > /root/.docker/config.json
+else
+  # GitLab container registry only
+  echo "{\"auths\":{\"registry.gitlab.com\":{\"auth\":\"${CI_REGISTRY_AUTH}\"}}}" > /root/.docker/config.json
+fi
